db8389dae428eb673a3bdbed629d9412097eb024
ShavJohn <shavarsh.garabedian@gmail.com>
2023-04-20 02:33:09 +0400

    Added animations

--- resources/js/components/home/home-header.vue
+++ resources/js/components/home/home-header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="responsive-background-image position-relative" style="background-image: url(http://www.nicdarkthemes.com/themes/hotel-inn/wp/demo/inn-suites/wp-content/uploads/sites/2/2021/12/parallax-26.jpg)">
-        
+        <h1 class="header-text-animation">Reserve your Holiday</h1>
     </div>
 </template>
 
