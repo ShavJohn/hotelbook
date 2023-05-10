a8beb4e7152489558ec435d3262e20a9ac21b724
ShavJohn <shavarsh.garabedian@gmail.com>
2023-05-10 22:48:40 +0400

    Updated

--- resources/js/components/room/room-page-header.vue
+++ resources/js/components/room/room-page-header.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="responsive-background-image position-relative" style="background-image: url(http://www.nicdarkthemes.com/themes/hotel-inn/wp/demo/inn-suites/wp-content/uploads/sites/2/2021/11/parallax-10.jpg)">
         <div class="background-image-filter">
-            <div>
-                
+            <div class="header-text-for-price">
+                <span>84$</span>
+                <span>/Per Night</span>
             </div>
         </div>
     </div>
