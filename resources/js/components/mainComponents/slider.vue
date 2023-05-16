cfb13cf1313c2fde8c1f33a72e051030d859afb3
ShavJohn <shavarsh.garabedian@gmail.com>
2023-05-16 16:48:01 +0400

    updated

---
+++ resources/js/components/mainComponents/slider.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="slider-container">
+        <button @click="prevSlide()"><font-awesome-icon icon="fa-solid fa-chevron-left" /></button>
+        <div class="slider-image-container">
+            <img class="slider-image" v-for="(image, key) in images" :src="image" :class="key === activeSlide && 'active-slider-image'">
+        </div>
+        <button @click="nextSlide()"><font-awesome-icon icon="fa-solid fa-chevron-right" /></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "slider",
+    data() {
+        return {
+            activeSlide: 0
+        }
+    },
+    props: {
+        images: {
+            type: Array,
+            required: true,
+        }
+    },
+    methods: {
+        prevSlide() {
+            this.activeSlide > 0 ? this.activeSlide-- : this.activeSlide =  this.images.length-1
+        },
+        nextSlide() {
+            this.activeSlide < this.images.length-1 ? this.activeSlide++ : this.activeSlide = 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/room/room-body.vue
+++ resources/js/components/room/room-body.vue
@@ -1,12 +1,33 @@
 <template>
-    <div>
-
+    <div class="room-page-body">
+        <div class="rooms-page-item">
+            <h2>Single Suit</h2>
+            <slider :images="images"/>
+        </div>
+        <div class="right-menu">
+            <room-date-table/>
+        </div>
     </div>
 </template>
 
 <script>
+import RoomDateTable from "./room-components/room-date-table";
+import Slider from "../mainComponents/slider";
 export default {
-    name: "room-body"
+    name: "room-body",
+    components: {Slider, RoomDateTable},
+    data() {
+        return {
+            images: [
+                'https://static.theprint.in/wp-content/uploads/2022/10/Hotel.jpg?compress=true&quality=80&w=376&dpr=2.6',
+                'https://www.gannett-cdn.com/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg',
+                'https://23c133e0c1637be1e07d-be55c16c6d91e6ac40d594f7e280b390.ssl.cf1.rackcdn.com/u/gpch/Park-Hotel-Group---Explore---Grand-Park-City-Hall-Facade.jpg',
+                'https://media.timeout.com/images/105938335/image.jpg',
+                'https://www.welcome-hotels.com/site/assets/files/35059/welcome_hotel_marburg_lobby_2k.2560x1600.jpg',
+                'https://hotelxtoronto.com/_novaimg/4906918-1481330_0_0_2200_1200_2200_1200.rc.jpg',
+            ]
+        }
+    }
 }
 </script>
 
